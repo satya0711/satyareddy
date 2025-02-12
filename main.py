25e35f1ffff620acd193ec8d0e462d2c429d0a94
satya0711 <satyareddy071196@gmail.com>
2025-02-12 06:55:08 +0000

    main.py has monified

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return 'Hello all welcome to python Flask world v1.0'
+   return 'Hello all to welcome to python Flask world v1.0'
 
 
 if __name__ == '__main__':
